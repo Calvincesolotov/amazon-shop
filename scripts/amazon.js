b31ec383e0eea4664547231f0715778822021b3f
Calvince Solotov <solotovtechn@gmail.com>
2023-11-11 09:35:02 +0300

    created cart.js,and added add to cart function

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -48,7 +48,7 @@ products.forEach((product) => {
     Added
   </div>
 
-  <button class="add-to-cart-button button-primary">
+  <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
     Add to Cart
   </button>
 </div>
@@ -56,7 +56,38 @@ products.forEach((product) => {
 
 })
 
-console.table(productsHTML);
+
 
 document.querySelector('.js-products-grid').
 innerHTML = productsHTML;
+
+
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) => {
+  button.addEventListener('click', () => {
+    
+    const productName = button.dataset.productName;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+
+      if(productName === item.productName){
+        matchingItem = item;
+      }
+    });
+
+    if(matchingItem){
+      matchingItem.quantity += 1;
+    }else{
+      cart.push({
+        productName:productName,
+        quantity:1
+      });
+
+    }
+
+    
+    console.table(cart);
+  });
+});
